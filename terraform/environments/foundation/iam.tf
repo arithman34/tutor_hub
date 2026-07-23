772bb89ab5deef941ec471441f1474c231a8c7e1
arithman34 <45328262+arithman34@users.noreply.github.com>
2026-07-23 17:56:53 +0100

    fix: allow deploy IAM role to manage lean ECS services

--- terraform/environments/foundation/iam.tf
+++ terraform/environments/foundation/iam.tf
@@ -130,6 +130,7 @@ data "aws_iam_policy_document" "github_deploy" {
     ]
     resources = [
       "arn:aws:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/${local.name}/*",
+      "arn:aws:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/${local.name}-lean/*",
     ]
   }
 
